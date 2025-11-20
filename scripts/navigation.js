77aa0b8b824dd8ec2b93ec72be07820128b7d7b2
princess wawa <123722709+princess-wawa@users.noreply.github.com>
2025-11-20 11:29:57 +0100

    feat (javascript): ajout des fonctions pour faire marcher les different boutons

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,116 @@
+function showcards(cards){
+    // recupere le container des cards
+    const cardcontainer = document.querySelector(".cartes-container")
+    cardcontainer.innerHTML = ''
+
+    for (const key of Object.keys(cards)){
+        currentcard = cards[key]
+        // initialise la card
+        let card = ""
+
+        //rajoute les elements dans la card
+        if ("tag" in currentcard){
+        card = `<div class="carte">
+                <div class="carte-partie-haute">
+                    <img src="${currentcard["image"]}">
+                    <div class="carte-titre-container">
+                        <span class="tag">${currentcard["tag"]}</span>
+                        <h2 class="div_présentation_texte">${key}</h2>
+                    </div>
+                </div>
+                <div class="div_description">
+                    <p>${currentcard["description"]}</p>
+                </div>
+            </div>`
+        } else {
+        card = `<div class="carte">
+                <div class="carte-partie-haute">
+                    <img src="${currentcard["image"]}">
+                    <div class="carte-titre-container">
+                        <h2 class="div_présentation_texte">${key}</h2>
+                    </div>
+                </div>
+                <div class="div_description">
+                    <p>${currentcard["description"]}</p>
+                </div>
+            </div>`
+        }
+        // ajoute la card dans le html
+        cardcontainer.insertAdjacentHTML("beforeend", card)
+    }
+
+}
+
+
+
+// fonction qui est appelée quand un des boutons du haut est appuyé
+function showcategory(sections) {
+    delete sections.couleur
+
+    const sectioncontainer = document.querySelector(".sections-container")
+    sectioncontainer.innerHTML = ""
+
+    // itere pour chaque sections principales
+    for (const key of Object.keys(sections)) {
+
+
+        // on cree une div pour mettre les bouttons
+        const sectiondiv = document.createElement("div");
+        sectiondiv.className = "section";
+
+        // cree un boutton
+        const newsectionbtn = document.createElement('button');
+        // donne un nom au boutton
+        newsectionbtn.textContent = key;
+
+        // on recupere les noms des children
+        let childrens = []
+        for (const clefs of Object.keys(sections[key])){
+            childrens.push(clefs);
+        }
+        // si les childrens sont des cards et non des sous catégories
+        if ("description" in sections[key][childrens[0]]){
+            // comme il n'y a pas de sous section, le bouton qu'on a crée est directement cliquable
+            newsectionbtn.className = "button-section-principale-actif"
+            // ajoute l'action du boutton
+            newsectionbtn.addEventListener('click', () =>{
+                showcards(sections[key])
+            });
+            // rajoute le bouton dans la div de la section
+            sectiondiv.appendChild(newsectionbtn)
+        // sinon (si les childrens sont des sous catégories)
+        } else {
+            // ---- on rajoute le boutton de la catégorie ----
+
+            // comme il y a des sections, le bouton qu'on a crée est inactif
+            newsectionbtn.className = "button-section-principale-inactif"
+            // rajoute le bouton dans la div de la section
+            sectiondiv.appendChild(newsectionbtn)
+
+            // ---- on rajoute les bouttons des sous catégories en dessous ----
+
+            // pour chaque sous catégorie
+            for (const children of childrens){
+                // cree un boutton
+                const newsectionbtn = document.createElement('button');
+                // comme il n'y a pas de sous section, le bouton est directement cliquable
+                newsectionbtn.className = "button-sous-sections"
+                // donne un nom au boutton
+                newsectionbtn.textContent = children;
+                // ajoute l'action du boutton
+                newsectionbtn.addEventListener('click', () =>{
+                    showcards(sections[key][children])
+                });
+                sectiondiv.appendChild(newsectionbtn) 
+            }
+        }
+
+        sectioncontainer.appendChild(sectiondiv)
+        
+    }
+};
+
+
 async function loadLayoutData() {
     // recupere le container des boutons
     const buttonContainer = document.querySelector("#bouttons-nav");
@@ -22,8 +135,8 @@ async function loadLayoutData() {
         newbtn.style.color = couleur;
 
         newbtn.addEventListener('click', () => {
-            console.log(couleur)
             document.documentElement.style.setProperty('--current-color', couleur);
+            showcategory(valeurs)
         });
         buttonContainer.appendChild(newbtn);
     }
